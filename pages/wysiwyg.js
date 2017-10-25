a68cd85eabe73c5ad0df20630397099a865d19f8
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-10-26 00:18:44 +0200

    Icons: updated

--- pages/wysiwyg.js
+++ pages/wysiwyg.js
@@ -30,12 +30,12 @@ class WysiwygPage extends Page {
       >
         <Wysiwyg
           items={[
-            { id: 1, type: 'title', content: '<h1>This is a title</h1>' },
-            { id: 2, type: 'paragraph', content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>' },
-            { id: 3, type: 'paragraph', content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>' },
-            { id: 4, type: 'paragraph', content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>' },
-            { id: 5, type: 'image', src: 'http://cine.netknowsl.netdna-cdn.com/cine/wp-content/uploads/2016/09/1-11.jpg', alt: 'Narcos' },
-            { id: 6, type: 'paragraph', content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>' }
+            // { id: 1, type: 'title', content: '<h1>This is a title</h1>' },
+            // { id: 2, type: 'paragraph', content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>' },
+            // { id: 3, type: 'paragraph', content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>' },
+            // { id: 4, type: 'paragraph', content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>' },
+            // { id: 5, type: 'image', src: 'http://cine.netknowsl.netdna-cdn.com/cine/wp-content/uploads/2016/09/1-11.jpg', alt: 'Narcos' },
+            // { id: 6, type: 'paragraph', content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>' }
           ]}
         />
       </Layout>
