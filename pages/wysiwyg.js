1adfc9cc8a3f8e6f9894e4dea26d449e40f26cb2
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-11-20 11:12:25 +0100

    Grid edition amendments

--- pages/wysiwyg.js
+++ pages/wysiwyg.js
@@ -42,7 +42,7 @@ class WysiwygPage extends Page {
           items={[
             { id: 1, type: 'text', content: '<h1>This is a title</h1>' },
             { id: 2, type: 'text', content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>' },
-            { id: 4, type: 'grid', content: [{ id: 41, type: 'embed', content: { src: 'https://staging.resourcewatch.org/embed/widget/7592da9e-8844-429d-9f2b-0147dde29ff5' } }, { id: 42, type: 'embed', content: { src: 'https://staging.resourcewatch.org/embed/widget/acf093a0-c627-4ca7-9963-9cf1cc0c563e' } }] },
+            // { id: 4, type: 'grid', content: [{ id: 41, type: 'embed', content: { src: 'https://staging.resourcewatch.org/embed/widget/7592da9e-8844-429d-9f2b-0147dde29ff5' } }, { id: 42, type: 'embed', content: { src: 'https://staging.resourcewatch.org/embed/widget/acf093a0-c627-4ca7-9963-9cf1cc0c563e' } }] },
             { id: 5, type: 'image', content: { src: 'https://media.istockphoto.com/photos/jordanian-dessert-at-sunrise-picture-id185099703?s=2048x2048', alt: 'Testing' } },
             { id: 6, type: 'text', content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>' }
           ]}
