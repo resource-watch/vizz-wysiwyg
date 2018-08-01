3bd489a3f20f416b257a7370417a4703a96e84b1
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2018-08-01 15:58:17 +0100

    Export the blocks

--- components/Wysiwyg/index.js
+++ components/Wysiwyg/index.js
@@ -1,1 +1,12 @@
 export { default } from 'components/Wysiwyg/Wysiwyg';
+
+// Blocks
+export { default as EmbedBlock } from 'components/Wysiwyg/Blocks/Embed';
+export { default as GridBlock } from 'components/Wysiwyg/Blocks/Grid';
+export { default as ImageBlock } from 'components/Wysiwyg/Blocks/Image';
+export { default as TextBlock } from 'components/Wysiwyg/Blocks/Text';
+export { default as VideoBlock } from 'components/Wysiwyg/Blocks/Video';
+export { default as EmbedEditionBlock } from 'components/Wysiwyg/Blocks/Edition/EmbedEdition';
+export { default as GridEditionBlock } from 'components/Wysiwyg/Blocks/Edition/GridEdition';
+export { default as ImageEditionBlock } from 'components/Wysiwyg/Blocks/Edition/ImageEdition';
+export { default as VideoEditionBlock } from 'components/Wysiwyg/Blocks/Edition/VideoEdition';
